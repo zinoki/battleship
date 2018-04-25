00171e2de0b26ccd8a731692d67300f43f9be080
Zachary <zinouye@gmail.com>
2018-04-24 19:27:40 -0700

    worked on creating new header component

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { hello } from './App.css';
-
+import { Header } from './Header.jsx';
 
 export default class Hello extends Component {
   render() {
     return (
       <div id={hello}>
+        <Header />
         Hello from react
       </div>
     );
---
+++ src/jsx/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>Hello from headerrrrr</div>
+  )
+}
+
+exports.Header = Header;
